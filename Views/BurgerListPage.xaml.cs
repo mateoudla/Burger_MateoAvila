d0bfde0af6f28e0032d7d12c0cb72f93761d29a3
mateoudla <117088528+mateoudla@users.noreply.github.com>
2023-01-17 20:22:10 -0500

    Update finale

--- Views/BurgerListPage.xaml.cs
+++ Views/BurgerListPage.xaml.cs
@@ -4,32 +4,47 @@ namespace Burger_MateoAvila.Views;
 
 public partial class BurgerListPage : ContentPage
 {
-    public event EventHandler AddBurger;
-
     public BurgerListPage()
     {
         InitializeComponent();
-        List<Burger> burger = App.BurgerRepo.GetAllBurgers();
-        burgerList.ItemsSource = burger;
-        MessagingCenter.Subscribe<BurgerItemPage>(this, "itemAdded", (sender) => OnUpdate(sender, EventArgs.Empty));
+        BindingContext = this;
+    }
+
+    public void OnItemAdded(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync(nameof(BurgerItemPage), true, new Dictionary<string, object>
+        {
+            ["Item"] = new Burger()
+        });
     }
 
     private void OnUpdate(object sender, EventArgs e)
     {
-        //fetch new items and update the Burgers property
         List<Burger> newBurgers = App.BurgerRepo.GetAllBurgers();
         burgerList.ItemsSource = newBurgers;
-        AddBurger?.Invoke(this, EventArgs.Empty);
-    }
-
-
-    async void OnItemAdded(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync(nameof(BurgerItemPage));
     }
 
     private void OnUpdateButtonClicked(object sender, EventArgs e)
     {
         OnUpdate(sender, e);
+    }
+
+    protected override void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        List<Burger> newBurgers = App.BurgerRepo.GetAllBurgers();
+        burgerList.ItemsSource = newBurgers;
+    }
+
+    private void OnCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        Burger burgers = e.CurrentSelection.FirstOrDefault() as Burger;
+        if (burgers == null)
+            return;
+        Shell.Current.GoToAsync(nameof(BurgerItemPage), true, new Dictionary<string, object>
+        {
+            {"Item", burgers }
+        });
+        ((CollectionView)sender).SelectedItem = null;
+
     }
 }
